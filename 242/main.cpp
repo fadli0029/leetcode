301e64c877e4733121a2c791f36d7d94b8e500f3
fadli0029 <fadlialim0029@gmail.com>
2024-08-01 21:02:36 -0700

    [ADD] more problems

--- 242/main.cpp
+++ 242/main.cpp
@@ -1,37 +1,24 @@
 #include "bits/stdc++.h"
+#include "helpers.cpp"
+#include <algorithm>
 using namespace std;
 
 class Solution {
-public:
+  public:
     bool isAnagram(string s, string t) {
-        if (s.length() != t.length()) {
+        if (s.size() != t.size())
             return false;
+
+        vector<int> count(26, 0);
+        for (int i = 0; i < s.size(); i++) {
+            count[int(s[i]) - int('a')]++;
+            count[int(t[i]) - int('a')]--;
         }
-        else {
-            // in the end every letters count must be 0
-            // iff for every occurence of a letter in s we increment
-            // its count and for that same letter in t we decrement
-            // its count in the vector.
-            vector<int> all_zeros(26, 0);
-            vector<int> count(26, 0); // count[0] is freq. of a, count[1] is freq. of b, etc.
-            for (size_t i=0; i<s.size(); i++) {
-                // compute the index
-                size_t s_curr_ch_index = int(s[i]) - int('a');
-                size_t t_curr_ch_index = int(t[i]) - int('a');
-                // update their count
-                count[s_curr_ch_index]++;
-                count[t_curr_ch_index]--;
-            }
-            return (count == all_zeros);
-        }
+        return all_of(count.begin(), count.end(), [](int i) { return i == 0; });
     }
 };
 
 int main() {
     Solution sol;
-    string s = "cat";
-    string t = "rat";
-    cout << sol.isAnagram(s, t) << endl;
     return 0;
 }
-
--- 49/main.cpp
+++ 49/main.cpp
@@ -1,60 +1,49 @@
 #include "bits/stdc++.h"
 #include "helpers.cpp"
+#include <string>
+#include <unordered_map>
 using namespace std;
 
-// NOTE: I timed myself to max 20 mins for medium leetcode problems.
-// If it that 20-minute mark is reached and I have not solvedd it,
-// I resolve to finding the solution.
-// I gave up for this problem.
 class Solution {
-public:
-    string encodeCount(const vector<int>& count) {
+  public:
+    string encode(vector<int> count) {
         string res = "";
-        for (const auto& freq : count) {
-            res += "#";
-            res += to_string(freq);
+        for (const auto &cnt : count) {
+            res += ("#" + to_string(cnt));
         }
         return res;
     }
-    vector<vector<string>> groupAnagrams(vector<string>& strs) {
-        if (strs.size() == 1) {
-            return {{strs}};
-        }
-        else {
-            // We can't do this: unordered_map<vector<int>, vector<string>> res;
-            // so we'll encode character count as a string: "#0#1#3...#0" for "bccc"
-            unordered_map<string, vector<string>> tracker;
-            for (size_t i=0; i<strs.size(); i++) {
-                vector<int> count(26, 0);
-                for (const char& ch : strs[i]) {
-                    count[int(ch) - int('a')]++;
-                }
-                string key = encodeCount(count); // encode `count`
+    vector<vector<string>> groupAnagrams(vector<string> &strs) {
+        // unordered_map<vector<int>, vector<string>> record;
+        // ^ this won't work, so we need to create our own "hashing" method.
 
-                if (tracker.find(key) != tracker.end()) {
-                    tracker[key].push_back(strs[i]);
-                }
-                else {
-                    vector<string> val = {strs[i]};
-                    tracker.insert(make_pair(key, val));
-                }
+        // we can encode the intended vector<int> above like so:
+        // #0#1#2#0#0#...#0 for bcc
+
+        unordered_map<string, vector<string>> record;
+        for (int i = 0; i < strs.size(); i++) {
+            vector<int> count(26, 0);
+            for (const auto &c : strs[i]) {
+                count[int(c) - int('a')]++;
             }
-            vector<vector<string>> res;
-            for (const auto& kv : tracker) {
-                res.push_back(kv.second);
+            string encoded_count = encode(count);
+
+            if (record.contains(encoded_count)) {
+                record[encoded_count].push_back(strs[i]);
+            } else {
+                record[encoded_count] = {strs[i]};
             }
-            return res;
         }
+
+        vector<vector<string>> result;
+        for (const auto &kv : record) {
+            result.push_back(kv.second);
+        }
+        return result;
     }
 };
 
 int main() {
     Solution sol;
-    vector<string> strs = {"eat","tea","tan","ate","nat","bat"};
-    vector<vector<string>> res = sol.groupAnagrams(strs);
-    for (const auto& item : res) {
-        printVector(item);
-    }
     return 0;
 }
-
